a7af8aa67ffa089dfd6d14b2b56f4d931d828caa
赵旋 <xuanzhao@88.com>
2021-03-26 16:14:23 +0800

    添加登录页logo和表单区域,且添加校验和toast

--- lib/pages/sign_in/sign_in.dart
+++ lib/pages/sign_in/sign_in.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_news/utils/utils.dart';
 import 'package:flutter_news/values/values.dart';
+import 'package:flutter_news/widgets/widgets.dart';
 
 class SignInPage extends StatefulWidget {
   @override
@@ -16,7 +17,7 @@ class _SignInPageState extends State<SignInPage> {
   // logo
   Widget _buildLogo() {
     return Container(
-      width: ftSetWidth(110),
+      width: ftSetWidth(106),
       margin: EdgeInsets.only(top: ftSetWidth(40 + 44.0)),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -83,8 +84,103 @@ class _SignInPageState extends State<SignInPage> {
     );
   }
 
+  // 登录表单
+  Widget _buildInputForm() {
+    return Container(
+      width: ftSetWidth(295),
+      // height: 204,
+      margin: EdgeInsets.only(top: ftSetHeight(49)),
+      child: Column(
+        children: [
+          // email input
+          inputTextEdit(
+            controller: _emailController,
+            keyboardType: TextInputType.emailAddress,
+            hintText: "Email",
+            marginTop: 0,
+          ),
+          // password input
+          inputTextEdit(
+            controller: _passController,
+            keyboardType: TextInputType.visiblePassword,
+            hintText: "Password",
+            isPassword: true,
+          ),
+
+          // 注册、登录 横向布局
+          Container(
+            height: ftSetHeight(44),
+            margin: EdgeInsets.only(top: ftSetHeight(15)),
+            child: Row(
+              children: [
+                // 注册
+                btnFlatButtonWidget(
+                  onPressed: () {
+                    Navigator.pushNamed(
+                      context,
+                      "/sign-up",
+                    );
+                  },
+                  gbColor: AppColors.thirdElement,
+                  title: "Sign up",
+                ),
+                Spacer(),
+                // 登录
+                btnFlatButtonWidget(
+                  onPressed: () {
+                    if (!ftIsEmail(_emailController.value.text)) {
+                      toastInfo(msg: '请正确输入邮件');
+                      return;
+                    }
+                    if (!ftCheckStringLength(_passController.value.text, 6)) {
+                      toastInfo(msg: '密码不能小于6位');
+                      return;
+                    }
+                  },
+                  gbColor: AppColors.primaryElement,
+                  title: "Sign in",
+                ),
+              ],
+            ),
+          ),
+          // Spacer(),
+
+          // Fogot password
+          Container(
+            height: ftSetHeight(22),
+            margin: EdgeInsets.only(top: ftSetHeight(20)),
+            child: FlatButton(
+              onPressed: () => {},
+              child: Text(
+                "Fogot password?",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: AppColors.secondaryElementText,
+                  fontFamily: "Avenir",
+                  fontWeight: FontWeight.w400,
+                  fontSize: ftSetFontSize(16),
+                  height: 1, // 设置下行高，否则字体下沉
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('SignInPage');
+    return Scaffold(
+      // resizeToAvoidBottomInset: false,
+      body: Center(
+        child: Column(
+          children: [
+            _buildLogo(),
+            _buildInputForm(),
+          ],
+        ),
+      ),
+    );
   }
 }
